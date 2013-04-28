18e0bf635fbfb890f7a210701881d1efbd1b4998
Aaron Morais <aaron.m@live.ca>
2013-04-28 19:26:00 -0400

    Remove jade templates. Configure node server to serve static files

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
-var express = require('express'),
-	jade = require('jade');
+var express = require('express');
+var app = express();
 
-var app = express.createServer();
+app.use(express.static(__dirname));
 
-app.configure(function() {
-    app.use(express.static(__dirname + '/static'));
-    app.use(express.bodyParser());
-});
-
-app.set('views', __dirname+'/template')
-app.set('view engine', 'jade');
-app.set('view options', {
-	layout:false
-});
-
-app.get('/', function(req, res, next) {
-    res.render('index');
-});
-app.get('/resume', function(req, res, next) {
-    res.render('resume');
-});
-app.get('/wip', function(req, res, next) {
-    res.render('newSite');
-});
-
-app.listen(9017);
+app.listen(process.env.PORT || 3000);
