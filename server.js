5f4db2a1d4237b259fef3d5f06be0828a37b7e79
Aaron Morais <aaron.m@live.ca>
2013-04-29 13:23:00 -0400

    Add routing for the main "/" and resume "/resume" page.

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req,res){
+ res.sendfile(__dirname + '/public/index.html');
+}); 
+
+app.get('/resume', function(req,res){
+ res.sendfile(__dirname + '/public/index.html');
+}); 
+
 app.listen(process.env.PORT || 3000);
